36f63e125173af8bcad9312cf9b184251e93f9e0
wensimin <hukaizhiyu2@gmail.com>
2020-07-15 21:43:54 +0800

    初始导入

---
+++ TranslateThread.py
@@ -0,0 +1,25 @@
+import io
+import threading
+import tkinter.messagebox
+
+from Api import Api
+
+
+# 图片转换翻译线程
+class TranslateThread(threading.Thread):
+    def __init__(self, image):
+        threading.Thread.__init__(self)
+        self.api = Api()
+        self.image = image
+
+    def run(self):
+        buffer = io.BytesIO()
+        self.image.save(buffer, format='PNG')
+        self.image.close()
+        # 图片转文本
+        sourceText = self.api.image2text(buffer.getvalue())
+        # 进行翻译
+        targetText = Api.translate(sourceText)
+        # TODO dialog
+        tkinter.messagebox.showinfo(title=None, message=targetText)
+        print(targetText)
